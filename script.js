ce38e7f85be0b60ca3725747e16dbd46a46ec918
Alec Sands <sandsalecv@gmail.com>
2017-06-03 08:56:18 -0500

    modified JSON syntax

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 console.log('hi');
 
-var jsonHTML = 'jsconcepts.json';
+var jsonHTML = 'https://alecsands.github.io/Flashcards/jsconcepts.json';
 var myRequest = new XMLHttpRequest();
 
 myRequest.open('GET', jsonHTML);
 myRequest.responsetype = 'json';
 myRequest.send();
 myRequest.onload = function() {
-  var programmingConcepts = myRequest.response;
+  var programmingConcepts = {};
+  programmingConcepts = myRequest.response;
   console.log(programmingConcepts);
+  console.log(programmingConcepts[0]);
   //populateHeaderConcept(programmingConcepts);
   return programmingConcepts;
 };
