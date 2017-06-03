d398e2944ce25fd5f54c67a9eea577f746fd6380
Alec Sands <sandsalecv@gmail.com>
2017-06-02 21:30:19 -0500

    basic script loaded

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log('hi');
+
+var jsonHTML = 'Flashcards/jsconcepts.json';
+var myRequest = new XMLHttpRequest();
+
+myRequest.open('GET', jsonHTML);
+myRequest.responsetype = 'json';
+myRequest.send();
+myRequest.onload = function() {
+  var programmingConcepts = myRequest.response;
+  console.log(programmingConcepts);
+  //populateHeaderConcept(programmingConcepts);
+  return programmingConcepts;
+};
+
+console.log(programmingConcepts);
